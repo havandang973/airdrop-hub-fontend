81c9252d6a2512d3333dd5c4921d80d1dd24d724
Ha Van <121249132+havandang973@users.noreply.github.com>
2025-11-20 10:45:35 +0700

    add coin

--- lib/helpers/coin.ts
+++ lib/helpers/coin.ts
@@ -40,13 +40,78 @@ export const COINS = [
         logo: 'https://s2.coinmarketcap.com/static/cloud/img/logo/polkadot/Polkadot_Logo_Animation_64x64.gif',
     },
     {
-        symbol: 'POLUSDT',
-        name: 'POL',
-        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/28321.png',
-    },
-    {
         symbol: 'LTCUSDT',
         name: 'LTC',
         logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2.png',
+    },
+    {
+        symbol: 'NEARUSDT',
+        name: 'NEAR',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/6535.png',
+    },
+    {
+        symbol: 'TRXUSDT',
+        name: 'TRX',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1958.png',
+    },
+    {
+        symbol: 'AVAXUSDT',
+        name: 'AVAX',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png',
+    },
+    {
+        symbol: 'SUIUSDT',
+        name: 'SUI',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/20947.png',
+    },
+    {
+        symbol: 'BCHUSDT',
+        name: 'BCH',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png',
+    },
+    {
+        symbol: 'LINKUSDT',
+        name: 'LINK',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png',
+    },
+    {
+        symbol: 'TONUSDT',
+        name: 'TON',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/11419.png',
+    },
+    {
+        symbol: 'UNIUSDT',
+        name: 'UNI',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png',
+    },
+    {
+        symbol: 'AAVEUSDT',
+        name: 'AAVE',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7278.png',
+    },
+    {
+        symbol: 'APTUSDT',
+        name: 'APT',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21794.png',
+    },
+    {
+        symbol: 'ATOMUSDT',
+        name: 'ATOM',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3794.png',
+    },
+    {
+        symbol: 'ARBUSDT',
+        name: 'ARB',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/11841.png',
+    },
+    {
+        symbol: 'WLDUSDT',
+        name: 'WLD',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/13502.png',
+    },
+    {
+        symbol: 'BCHUSDT',
+        name: 'BCH',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/13502.png',
     },
 ];
